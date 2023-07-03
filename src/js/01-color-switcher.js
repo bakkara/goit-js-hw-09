352d9796e8e3f18a8e7da4f4c86a3f5bc334ed96
bakkara <ane4karymar@gmail.com>
2023-07-03 09:25:49 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let timerId = null;
+
+buttonStart.addEventListener('click', startChangeColor);
+
+
+buttonStop.addEventListener('click', stopChangeColor);
+
+function startChangeColor() {
+    buttonStart.disabled = true;
+    timerId = setInterval(changeColor, 1000);
+    buttonStop.disabled = false;
+}
+
+function changeColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+function stopChangeColor() {
+    buttonStop.disabled = true;
+    clearInterval(timerId);
+     buttonStart.disabled = false;
+}
 
